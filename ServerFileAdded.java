a075d1045de990edf9aef80024c1b8ef23c856a8
tapanesh <dashtapanesh@gmail.com>
2014-01-22 07:09:37 -0600

    added a new line in 3rd statement .

--- ServerFileAdded.java
+++ ServerFileAdded.java
@@ -1,3 +1,4 @@
 Hi This is the server file added  from the Server side repo.
 I just added a new line in the middle.
+I just added another line into the file from the server repo. // By Server Repo Tapanesh_Dash_Server_Repo
 Let's check whether it is available it in the local repo after checkout ie. PULL .
